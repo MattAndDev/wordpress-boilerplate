1b5fbb14cfbe7cebca3126b526b38caaebd3e230
Matteo Maronati <matteo@gentleman-studio.de>
2016-12-14 23:32:21 +0100

    Up: scaffolding for data model

---
+++ public/app/themes/.source_theme-name/src/code/inc/data-model/register_post_types.php
@@ -0,0 +1,10 @@
+<?php
+
+// =
+// Use this hook to register post types
+
+function register_post_types() {
+
+}
+
+add_action('init', 'register_post_types');
---
+++ public/app/themes/.source_theme-name/src/code/inc/data-model/register_taxonomies.php
@@ -0,0 +1,10 @@
+<?php
+
+// =
+// Use this hook to register post types
+
+function register_taxonomies() {
+
+}
+
+add_action('init', 'register_taxonomies');
